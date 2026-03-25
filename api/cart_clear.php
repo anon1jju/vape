2736956a38c8e8e930ae32b211ff7b12412af609
gandalf <104222912+anon1jju@users.noreply.github.com>
2026-03-25 16:47:51 +0700

    Delete api directory

--- api/cart_clear.php
+++
@@ -1,8 +0,0 @@
-<?php
-date_default_timezone_set('Asia/Jakarta');
-
-$cartFile = __DIR__ . '/../data/cart.json';
-file_put_contents($cartFile, json_encode([], JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE));
-
-header('Location: ../kasir.php?msg=' . urlencode('Keranjang dikosongkan.'));
-exit;
